89c570596bc6dd7bb579bbfa617f81ffcfa02bea
leosilvadev <leosilvadev@gmail.com>
2015-05-17 22:04:13 -0300

    configuring to run on heroku

--- src/main/groovy/br/leosilvadev/gchat/Application.groovy
+++ src/main/groovy/br/leosilvadev/gchat/Application.groovy
@@ -1,24 +1,44 @@
 package br.leosilvadev.gchat
 
-import groovy.json.JsonBuilder;
-
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.context.embedded.FilterRegistrationBean
-import org.springframework.boot.context.web.SpringBootServletInitializer
 import org.springframework.context.annotation.Bean
 import org.springframework.orm.jpa.support.OpenEntityManagerInViewFilter
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean
 
-import com.fasterxml.jackson.annotation.JsonInclude.Include;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.annotation.JsonInclude.Include
+import com.fasterxml.jackson.databind.ObjectMapper
 
 @SpringBootApplication
 class Application {
 	
 	static void main(String[] args) throws Exception {
+		configurePort()
+		configureDatabase()
+		
 		SpringApplication.run(Application.class, args)
+	}
+
+	private static configureDatabase() {
+		if ( System.getenv().DATABASE_URL ) {
+			URI dbUri = new URI(System.getenv().DATABASE_URL)
+			String username = dbUri.getUserInfo().split(":")[0]
+			String password = dbUri.getUserInfo().split(":")[1]
+			String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath()
+			
+			System.setProperty("spring.datasource.url", dbUrl)
+			System.setProperty("spring.datasource.username", username)
+			System.setProperty("spring.datasource.username", password)
+		}
+	}
+
+	private static configurePort() {
+		if ( System.getenv().PORT ) {
+			String webPort = System.getenv().PORT
+			System.setProperty("server.port", webPort)
+			
+		}
 	}
 	
 	Application(){
